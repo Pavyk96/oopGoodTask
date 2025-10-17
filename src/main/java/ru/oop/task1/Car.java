d59de065976995ac05a4e0acf8f90971c063b741
Mezev Daniil <pepethefrog27@mail.com>
2025-10-17 13:08:49 +0500

    Задача 1

---
+++ src/main/java/ru/oop/task1/Car.java
@@ -0,0 +1,14 @@
+package ru.oop.task1;
+
+/**
+ * Car
+ *
+ * @author Daniil Mezev
+ */
+public class Car {
+    /**
+     * Двигаться на машине насколько это возможно
+     */
+    public void moveTo(Person person, Position destination) {
+    }
+}
--- src/main/java/ru/oop/task1/MainTask1.java
+++ src/main/java/ru/oop/task1/MainTask1.java
@@ -22,7 +22,10 @@ public class MainTask1 {
      * Переехать из текущего места в заданную точку
      */
     public void moveTo(Person person, Position destination) {
-        // TODO
+        Car car = new Car();
+        car.moveTo(person, destination);
+        person.walk(destination);
         assert person.getPosition() == destination;
     }
+
 }
